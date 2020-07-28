21e5d4ffcafa128f4ffce46f5dac15f542a60241
Ihor <66252420+Grigoryan-not-available@users.noreply.github.com>
2020-07-28 13:32:54 +0300

    Version 1.1

--- Asterisk/backup_asterisk.sh
+++ Asterisk/backup_asterisk.sh
@@ -1,9 +1,24 @@
+
 #!/bin/bash
 date_for_backup=$(date +'%d.%m.%Y')
 dest_dir=/mnt/backup/asterisk/$date_for_backup
 sudo yum install -y cifs-utils
 mkdir /mnt/backup
 mount -t cifs //192.168.0.201/pilot_test_asterisk_backup /mnt/backup -o user=duplicity,password=Backup_123
-mkdir $dest_dir
+mkdir -p $dest_dir/etc/asterisk
+mkdir -p $dest_dir/var/lib/asterisk
 ls /mnt/backup
-sudo cp -r /etc/asterisk/* $dest_dir
+sudo cp -r /etc/asterisk/* $dest_dir/etc/asterisk
+sudo cp -r /var/lib/asterisk/* $dest_dir/var/lib/asterisk
+
+
+
+
+
+
+
+
+
+
+
+
