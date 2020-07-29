9e07798d579f99543bf0273fc71ba5798649aa46
Ihor <66252420+Grigoryan-not-available@users.noreply.github.com>
2020-07-29 14:52:01 +0300

    Version 2.0

--- Zabbix/backup_zabbix.sh
+++ Zabbix/backup_zabbix.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 date_for_backup=$(date +'%d.%m.%Y')
-sudo yum install -y cifs-utils
-mkdir /mnt/backup
-mount.cifs //192.168.0.201/pilot_test_zabbix_back /mnt/backup -o user=duplicity,password=Backup_123
-ls /mnt/backup
-sleep 5
 name_backup=zabbix_$date_for_backup.sql
-dest_dir=/mnt/backup/zabbix/$date_for_backup
-mkdir $dest_dir
-mysqldump -u root -p1!Cheklanin483 zabbix > $dest_dir/$name_backup
-cp -r /etc/php-fpm.d $dest_dir
-cp -r /etc/my.cnf.d $dest_dir
-cp -r /etc/zabbix $dest_dir
-cp -r /etc/nginx $dest_dir
-umount //192.168.0.201/pilot_test_zabbix_back
+sshpass -p 'Gt@85pass' ssh ihor@192.168.0.86 "mkdir /mnt/reserve_backup/Zabbix/$date_for_backup"
+mysqldump -u root -p1!Cheklanin483 zabbix > $name_backup
+sshpass -p 'Gt@85pass' scp -r $name_backup /etc/php-fpm.d /etc/my.cnf.d /etc/zabbix /etc/nginx ihor@192.168.0.86:/mnt/reserve_backup/Zabbix/$date_for_backup
+rm -rf $name_backup
+sshpass -p 'Gt@85pass' ssh ihor@192.168.0.86 "chmod -R 777 /mnt/reserve_backup/Zabbix/$date_for_backup"
 echo "Finished"
